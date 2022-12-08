82eaf9a81a96e1725c1f46c31be8ef5e417452c7
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-08 14:05:06 -0400

    @types and ignores something

--- express-typescript/src/index.ts
+++ express-typescript/src/index.ts
@@ -1,14 +1,15 @@
-import express from 'express'
-const app =express()
-app.use(express.json())
+import express from "express";
+const app = express();
+app.use(express.json());
 
-const PORT = 3000
+const PORT = 3000;
 
-app.get('/ping',(res,req)=>{
-console.log("Hola mundo")
-res.send('Ping')
-})
+// Parametro bajo ignora
+app.get("/ping", (_req, res) => {
+  console.log("Hola mundo");
+  res.send("Ping");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
