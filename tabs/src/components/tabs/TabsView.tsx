9e8c7fd1140e78ce9876df3e1281e26c4cf8a6a3
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-08 11:30:14 -0400

    styles

--- tabs/src/components/tabs/TabsView.tsx
+++ tabs/src/components/tabs/TabsView.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import { TabItem } from "./types/types";
+import  { TabItem } from "./types/types";
+import styles from "./tabsView.module.css";
+import classnames from "classnames";
 
 interface TabsViewProps {
   items: TabItem[];
   onChange: (item: TabItem) => void;
 }
+
 export default function TabsView({ items }: TabsViewProps) {
   const [selected, setSelected] = useState<number | null>(
     items.length >= 0 ? 0 : null
@@ -20,7 +23,7 @@ export default function TabsView({ items }: TabsViewProps) {
 
   return (
     <div>
-      <div >
+      <div className={styles.tabsContainer}>
         {items.map((item, index) => (
           <TabView
             index={index}
@@ -31,7 +34,7 @@ export default function TabsView({ items }: TabsViewProps) {
         ))}
       </div>
       <div>
-        <div >
+        <div className={styles.contentContainer}>
           {items.map((item, index) => (
             <>
               {selected === index && (
@@ -46,19 +49,20 @@ export default function TabsView({ items }: TabsViewProps) {
     </div>
   );
 }
+
 interface TabViewProps {
-    index: number;
-    active: boolean;
-    item: TabItem;
-    onClick: (index: number) => void;
-  }
-  
-  function TabView({ index, active, item, onClick }: TabViewProps) {
-    return active ? (
-      <div>{item.title}</div>
-    ) : (
-      <button  onClick={() => onClick(index)}>
-        {item.title}
-      </button>
-    );
-  }
+  index: number;
+  active: boolean;
+  item: TabItem;
+  onClick: (index: number) => void;
+}
+
+function TabView({ index, active, item, onClick }: TabViewProps) {
+  return active ? (
+    <div className={classnames(styles.tab, styles.activeTab)}>{item.title}</div>
+  ) : (
+    <button className={styles.tab} onClick={() => onClick(index)}>
+      {item.title}
+    </button>
+  );
+}
