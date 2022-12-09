44eb16376c0681aafff38a5448ec60c135396481
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-09 11:31:06 -0400

    filter by id

--- express-typescript/src/routes/diaries.ts
+++ express-typescript/src/routes/diaries.ts
@@ -6,6 +6,10 @@ const router = express.Router();
 router.get("/", (_req, res) => {
   res.send(diaryServices.getEntriesWithoutSenstitiveInfo());
 });
+router.get("/:id", (req, res) => {
+  const diary = diaryServices.findById(Number(req.params.id));
+  return diary != null ? res.send(diary) : res.sendStatus(404);
+});
 
 router.post("/", (_req, res) => {
   res.send("Saving a diaries ");
--- express-typescript/src/services/diaryServices.ts
+++ express-typescript/src/services/diaryServices.ts
@@ -5,9 +5,15 @@ import diaryData from "./diaries.json";
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
 
 export const getEntries = (): DiaryEntry[] => diaries;
+
+export const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
+};
+
 export const getEntriesWithoutSenstitiveInfo =
   (): NonSensitiveInfoDiaryEntry[] => {
-  return  diaries.map(({ id, date, visibiliity, wather }) => {
+    return diaries.map(({ id, date, visibiliity, wather }) => {
       return {
         id,
         date,
